b0a77635c06a13ac88531b745ea118f910df4c4c
zt <731315163@qq.com>
2018-05-17 10:22:54 +0800

    ubunt on windows

---
+++ buildandroid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+unity=/mnt/d/Soft/Unity/5.6.3f1/Editor/Unity.exe
+unityproject=D:/Project/UnityProject/TowerCrush
+method=BuildProject.BuildAndroid
+apktoolWin=D:\Soft\ApkTool
+apktoolLin=/mnt/d/Soft/ApkTool
+$unity -projectPath $unityproject -executeMethod $method -quit
+ 
+echo "Apk生成完毕"
